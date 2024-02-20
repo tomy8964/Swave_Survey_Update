97da415d48259e24766b9ce570777904e49a1ad0
ham <tomy8964@naver.com>
2024-02-20 21:29:49 +0900

    분산락 AOP 수정
    @Target({ElementType.METHOD})
    @Retention(RetentionPolicy.RUNTIME)

--- surveyanalyze/src/main/java/com/example/surveyanalyze/redis/lock/DistributedLock.java
+++ surveyanalyze/src/main/java/com/example/surveyanalyze/redis/lock/DistributedLock.java
@@ -1,10 +1,12 @@
 package com.example.surveyanalyze.redis.lock;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@Aspect
-@Component
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DistributedLock {
     String value() default "";
 }
--- surveydocument/src/main/java/com/example/surveydocument/redis/lock/DistributedLock.java
+++ surveydocument/src/main/java/com/example/surveydocument/redis/lock/DistributedLock.java
@@ -1,10 +1,13 @@
 package com.example.surveydocument.redis.lock;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@Aspect
-@Component
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DistributedLock {
     String value() default "";
 }
