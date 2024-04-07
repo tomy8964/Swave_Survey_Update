a4cdde938a09f6aced48dff44b5f40e1ca39fe24
ham <tomy8964@naver.com>
2024-04-07 18:43:58 +0900

    진정성 검사 실패 커스텀 예외 수정
    진정성 검사 실패 커스텀 예외 메세지 오류 수정

--- surveyanswer/src/main/java/com/example/surveyanswer/survey/exception/InvalidReliabilityException.java
+++ surveyanswer/src/main/java/com/example/surveyanswer/survey/exception/InvalidReliabilityException.java
@@ -1,7 +1,7 @@
 package com.example.surveyanswer.survey.exception;
 
 public class InvalidReliabilityException extends RuntimeException{
-    private static final String MESSAGE = "회원 정보를 찾을 수 없습니다 ";
+    private static final String MESSAGE = "진정성 검사에 실패한 응답입니다.";
 
     public InvalidReliabilityException() {
         super(MESSAGE);
